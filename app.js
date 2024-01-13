5365e5d2b035fc3df9fe5daaec2e2e00149b43db
Prabhat <prabhatkumarnagma@gmail.com>
2024-01-13 18:38:47 +0530

    episode-02

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const heading = React.createElement(
-  "h1",
-  { id: "htag", class: "hclass" },
-  "Hello React!"
-);
+import React from "react";
+import ReactDOM from "react-dom";
+const heading = React.createElement("div", { id: "htag", class: "hclass" }, [
+  React.createElement("h1", { id: "h1tag" }, "Prabhat Yadav"),
+  React.createElement("h2", { id: "h2tag" }, "I love My India"),
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
