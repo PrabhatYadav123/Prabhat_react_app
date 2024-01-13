e7b2c66dbcf8656c54c9db1893e6ea225ea618ab
Prabhat <prabhatkumarnagma@gmail.com>
2024-01-13 22:23:35 +0530

    episode-03

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
-const heading = React.createElement("div", { id: "htag", class: "hclass" }, [
-  React.createElement("h1", { id: "h1tag" }, "Prabhat Yadav"),
-  React.createElement("h2", { id: "h2tag" }, "I love My India"),
-]);
+
+const heading = React.createElement("h1", { id: "heading" }, "Namste React");
+//React Element
+const jsxheading = (
+  <h1 id="heading" className="head">
+    "Namste React By Sachin Yadav"
+  </h1>
+);
+//Functional React Component
+const Heading = () => {
+  return <h1>Namste React</h1>;
+};
+
+const Heading1 = () => (
+  <div id="heading">
+    {jsxheading}
+    <Heading />
+    {/* <Heading>gi</Heading> */}
+    {/* {Heading()} */}
+    <h1 className="head">Namste React Course</h1>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+// root.render(heading);
+// root.render(jsxheading);
+root.render(<Heading1 />);
